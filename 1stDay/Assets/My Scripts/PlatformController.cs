06b16f026093bac3e6d99eccdc88a4f6b0d32f29
Clarence Marak <marak.clarence@student.ccm.edu>
2013-09-23 23:01:05 -0400

    Add accel/decel; change jump

--- 1stDay/Assets/My Scripts/PlatformController.cs
+++ 1stDay/Assets/My Scripts/PlatformController.cs
@@ -24,16 +24,25 @@ public class PlatformController : MyDerivedMono
     public Control MoveLeft,
                    MoveRight,
                    Jump;
+
+    public float XAccel = 30,
+                 XDecel = 60,
+                 XMaxVel = 60,
+                 XVel = 0,
+                 YAccel = -30,
+                 YTermVel = -60,
+                 YJump = 60,
+                 YVel = 0;
     
-    public float Gravity = 9,
-                 JumpStrength = 30,
-                 MoveSpeed = 15;
-
-    public int JumpDuration = 10,
-               JumpFor = 0;
-
-    public bool CanJump = true;
-
+//    public float Gravity = 9,
+//                 JumpStrength = 30,
+//                 MoveSpeed = 15;
+//
+//    public int JumpDuration = 10,
+//               JumpFor = 0;
+//
+//    public bool CanJump = true;
+//
     private CharacterController controller;
     CollisionFlags prevFlags;
 
@@ -50,33 +59,132 @@ public class PlatformController : MyDerivedMono
     {
         Vector3 moveVec = Vector3.zero;
 
-        if (MoveLeft.IsActive)
-        {
-            moveVec.x -= MoveSpeed;
-        }
-
-        if (MoveRight.IsActive)
-        {
-            moveVec.x += MoveSpeed;
-        }
-
         if (prevFlags.Has(CollisionFlags.CollidedBelow))
         {
-            if (Jump.KeyDown)
-            {
-                JumpFor = JumpDuration;
-            }
+			if (Jump.KeyDown)
+			{
+				YVel = YJump;
+			}
+			else
+			{
+				YVel = 0;
+				if (MoveLeft.IsActive)
+				{
+					if (XVel > -1*XMaxVel)
+					{
+						if (XVel > 0)
+						{
+							XVel -= (XDecel * Time.deltaTime);
+							if (XVel < 0)
+							{
+								XVel = 0;
+							}
+						}
+						else
+						{
+							XVel -= (XAccel * Time.deltaTime);
+							if (XVel<-1*XMaxVel)
+							{
+								XVel = -1*XMaxVel;
+							}
+						}
+					}
+				}
+				else if (MoveRight.IsActive)
+				{
+					if (XVel < XMaxVel)
+					{
+						if (XVel < 0)
+						{
+							XVel += (XDecel * Time.deltaTime);
+							if (XVel > 0)
+							{
+								XVel = 0;
+							}
+						}
+						else
+						{
+							XVel += (XAccel * Time.deltaTime);
+							if (XVel > XMaxVel)
+							{
+								XVel = XMaxVel;
+							}
+						}
+					}
+				}
+				else
+				{
+					if (XVel < 0)
+					{
+						XVel += (XDecel * Time.deltaTime);
+						if (XVel > 0)
+						{
+							XVel = 0;
+						}
+					}
+					else if (XVel > 0)
+					{
+						XVel -= (XDecel * Time.deltaTime);
+						if (XVel < 0)
+						{
+							XVel = 0;
+						}
+					}
+				}
+			}
         }
-        else
-        {
-            moveVec.y -= Gravity;
-        }
+		else
+		{
+			YVel += (YAccel * Time.deltaTime);
+			if (YVel < YTermVel)
+			{
+				YVel = YTermVel;
+			}
+		}
+		
+		if (prevFlags.Has(CollisionFlags.CollidedAbove))
+		{
+			if (YVel > 0)
+			{
+				YVel = 0;
+			}
+		}
+		
+		if (prevFlags.Has(CollisionFlags.CollidedSides))
+		{
+			XVel = 0;
+		}
+		
+		moveVec.x += XVel * Time.deltaTime;
+		moveVec.y += YVel * Time.deltaTime;
 
-        if (JumpFor > 0)
-        {
-            moveVec.y += JumpStrength;
-            JumpFor -= 1; 
-        }
+        //if (MoveLeft.IsActive)
+        //{
+        //    moveVec.x -= MoveSpeed;
+        //}
+
+        //if (MoveRight.IsActive)
+        //{
+        //    moveVec.x += MoveSpeed;
+        //}
+
+        //if (prevFlags.Has(CollisionFlags.CollidedBelow))
+        //{
+        //    if (Jump.KeyDown)
+        //    {
+        //        JumpFor = JumpDuration;
+        //    }
+        //}
+        //else
+        //{
+        //    moveVec.y -= Gravity;
+        //}
+
+        //if (JumpFor > 0)
+        //{
+        //    moveVec.y += JumpStrength;
+        //    JumpFor -= 1; 
+        //}
 
         prevFlags = controller.Move(moveVec * Time.deltaTime);
     }
